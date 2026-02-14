5afd49770c7f04cc7858f24ee50f22a12d045518
Christopher Thomas <smiley.culture@gmail.com>
2026-02-14 13:01:10 +0000

    Revert "Update AndroidPluginCompiler.php"
    This reverts commit b52cd343245b18336fbc97fb04bfe351d1adba73.

--- src/Plugins/Compilers/AndroidPluginCompiler.php
+++ src/Plugins/Compilers/AndroidPluginCompiler.php
@@ -554,9 +554,6 @@ class AndroidPluginCompiler
             }
             $attrs[] = "android:foregroundServiceType=\"{$type}\"";
         }
-        if (isset($service['process'])) {
-            $attrs[] = "android:process=\"{$service['process']}\"";
-        }
 
         $attrString = implode("\n            ", $attrs);
 
